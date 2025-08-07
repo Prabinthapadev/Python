b57830099b0299d9f0cbff575d952fc78eae9f85
Prabinthapadev <168621088+Prabinthapadev@users.noreply.github.com>
2025-08-07 08:12:43 +0545

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,37 @@
+
+# here id method helps to know the memory location
+# print(id(num))
+# by using type method we can find the type of the variable.
+# print(type(num))
+
+# Datatypes in python:
+# None : here we don't assign any value to the variable
+
+# Numeric:here it consist of datatype such as:
+# int eg: num = 2
+# float eg: num = 4.5
+# complex  eg:  it is in the form of complex type  num = 3+4j
+# boolean  eg: isFollow = true
+
+
+# List: eg: lst = [1,2,3,4,5,6]
+
+# Set: eg: s = {1,2,3,4,5,6}
+
+# Tuple: eg: t = (1,2,3,4,5);
+
+# String: eg: name = "Prabin"
+
+# Range: eg: eg: range(10)
+# we can print the number to 10 using list eg:
+print(list(range(0,10)))
+# if we want to print even number  only then range accept 3 parameters eg:
+print(list(range(2,11,2)))
+
+# Dictionary: here we can assign key for each values eg:
+# d = {'prabin':'Iphone','sasin':'mac','ashok':'hp-laptop'}
+# print(d.values())
+# print(d.keys())
+# for getting a particular value
+# print(d['sasin'])
+# print(d.get('sasin'))
