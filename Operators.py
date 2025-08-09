a26b6b03e6b2cb72fcfb902a15b6ab13b687580c
Prabinthapadev <168621088+Prabinthapadev@users.noreply.github.com>
2025-08-09 20:44:31 +0545

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,62 @@
+
+# performing the different operations by using the availiable operators in python
+# Arithmetic Operator:operators like:addition,subtraction,multiply,divide,modolus etc.
+# eg:
+# n1 = 4
+# n2 = 5
+# print(n1+n2);
+# print(n1-n2);
+# print(n1*n2);
+# print(n1/n2);
+# print(n1%n2);
+
+# assignment operators
+# these operators are used to assign value to the variable eg:
+# a = 10
+# b=0
+# b += a
+# print(b)
+
+# logical operators
+# these operators are used in the logical portions eg:
+# a = 2
+# b = 3
+# if a == b:
+#     {
+#         print("A is equal to b")
+#     }
+# else:
+#     print("B is greater than a")
+
+# program to check if user input age eligible for voting.
+#
+# age = int(input("Enter your age: "))
+# if age>=18:
+#         print("You can vote")
+#
+# else:
+#     print("You cannot vote")
+
+
+# Type Casting
+
+# num = 10;
+# value = float(num)
+# print(value)
+# n = 2.44
+# m = int(n);
+# print(m)
+
+# typecasting string to boolean
+
+# str ="Prabin"
+# isinput = bool(str);
+# print(isinput)
+# here if the string contains value then it returns true instead if it is null then it returns false
+
+# to take input from user we use input() function/method
+# example
+# num = int(input("Enter a number:"))
+# print(num)
+
+
