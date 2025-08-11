13321be57485ebbb331a2123d76693cc137b46a0
Prabinthapadev <168621088+Prabinthapadev@users.noreply.github.com>
2025-08-11 17:27:40 +0545

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,13 @@
+num1 = 10
+num2 = 15
+
+add = num1+num2
+sub = num1-num2
+mul = num1*num2
+div = num1/num2
+mod = num1%num2
+print(f"Addition is {add}")
+print(f"Subtraction is {sub}")
+print(f"Multiplication is {mul}")
+print(f"Division is {div}")
+print(f"Modolus is {mod}")
