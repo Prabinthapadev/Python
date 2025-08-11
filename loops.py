a931522f2e5f52997245e97c8e255f20da27eb8c
Prabinthapadev <168621088+Prabinthapadev@users.noreply.github.com>
2025-08-11 17:29:34 +0545

    conditional statements in python

---
+++ loops.py
@@ -0,0 +1,44 @@
+# to check greatest among three numbers
+# num1 = 2
+# num2 = 4
+# num3 = 1
+
+# if num1>num2 and num1>num3:
+#     print(f"{num1} is greater")
+
+# elif num2>num1 and num2>num3:
+#     print(f"{num2} is greater")
+# else:
+#     print(f"{num3} is greater")
+
+# for loop and while loop 
+
+    # i = 0
+    # while i<5:
+    #     print("Hello World")
+    #     i+=1
+
+# program to find number is even or odd
+# num = int(input("Enter a number:"))
+# if num % 2 == 0:
+#     print(f"{num} is even")
+# else:
+#     print(f"{num} is odd")
+
+# program to find number is prime or not
+# num = int(input("Enter a number"))
+# count = 0
+# for i in range(1,num+1):
+#     if num %i == 0:
+#         count+=1
+         
+# if count == 2:
+#     print(f"{num} is prime")
+# else:
+#     print(f"{num} is composite")
+
+# program to find the factorial to 2 upto 100
+# for i in range(2,100+1):
+#     if i%2 == 0:
+#         print(i)
+#         continue
