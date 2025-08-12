b4d44d28003ace023876a3f60e9e52665b3638cb
Prabinthapadev <168621088+Prabinthapadev@users.noreply.github.com>
2025-08-12 07:12:34 +0545

    function and modules in python

---
+++ function.py
@@ -0,0 +1,52 @@
+# function in python
+# function helps to reuse the code in the program
+
+# defining first  hello world function
+# def greet():
+#     print("Hello World")
+
+# greet()
+
+# functions with parameters
+
+# def greet(name):
+#     print(f"hi {name}")
+
+# greet("prabin")
+
+
+# function with parameters and returning value
+
+# def add(a,b):
+#     return a+b
+
+# result = add(5,3)
+# print(f"result = {result}")
+
+
+
+# modules in python
+# modules are the files which contains inbuilt functions which performs a certain tasks
+
+# import math
+# num = 16
+# root = int(math.sqrt(num))
+# print(f"root ={root}")
+
+# we can also use specific functions we want
+# from math import sqrt,pi
+# print(sqrt(25))
+# print(pi)
+
+
+# defining our own modules example
+
+# index.py
+
+# def add(a,b)
+#    return a+b
+
+
+# in addition.py
+# import index
+# print(index.add(4,5))
